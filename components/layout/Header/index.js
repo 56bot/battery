1ee1219dbcaccededd7121903af68cbbfb0e6991
T <timur@56.digital>
2020-07-04 23:56:09 -0400

    Lock header scroll + ESC to close + Negative space 2 close

--- components/layout/Header/index.js
+++ components/layout/Header/index.js
@@ -1,5 +1,6 @@
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import FixedHeader from "./FixedHeader";
 import ExpandedHeader from "./ExpandedHeader";
@@ -7,6 +8,27 @@ import ExpandedHeader from "./ExpandedHeader";
 const Header = () => {
   const router = useRouter();
   const [headerExpanded, setHeaderExpanded] = useState(false);
+
+  useEffect(() => {
+    if (headerExpanded) disableBodyScroll();
+    else enableBodyScroll();
+  }, [headerExpanded]);
+
+  useEffect(() => {
+    const checkForEscButton = (e) => {
+      if (e.keyCode === 27) closeHeader();
+    };
+
+    document.addEventListener("keyup", checkForEscButton);
+
+    return () => {
+      document.removeEventListener("keyup", checkForEscButton);
+    };
+  }, []);
+
+  const closeHeader = () => {
+    setHeaderExpanded(false);
+  };
 
   return (
     <header className={`header expanded-${headerExpanded} c12`}>
@@ -16,12 +38,26 @@ const Header = () => {
         setHeaderExpanded={setHeaderExpanded}
       />
 
+      {headerExpanded && (
+        <div className="header-collapser" onClick={closeHeader} />
+      )}
+
       <style jsx>{`
         header {
           position: static;
           top: 0;
           display: inline;
           left: 0;
+        }
+
+        .header-collapser {
+          z-index: 4;
+          position: fixed;
+          height: 100vh;
+          width: 100vw;
+          top: 0;
+          left: 0;
+          cursor: pointer;
         }
 
         header.expanded-false {
